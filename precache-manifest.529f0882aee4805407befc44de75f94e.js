dbec5b3e5106f7785d633d9c186b783c330ac15b
Yurii <yurii.oleshko@gmail.com>
2019-02-26 16:06:10 +0200

    Updates

---
+++ precache-manifest.529f0882aee4805407befc44de75f94e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Table_tyt/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fcbac7577b60b76d0010",
+    "url": "/Table_tyt/static/js/runtime~main.fcbac757.js"
+  },
+  {
+    "revision": "efe005d5f8b86abc721a",
+    "url": "/Table_tyt/static/js/main.efe005d5.chunk.js"
+  },
+  {
+    "revision": "3a011211a0a60ecedbb3",
+    "url": "/Table_tyt/static/js/2.3a011211.chunk.js"
+  },
+  {
+    "revision": "efe005d5f8b86abc721a",
+    "url": "/Table_tyt/static/css/main.59ddaf87.chunk.css"
+  },
+  {
+    "revision": "3a011211a0a60ecedbb3",
+    "url": "/Table_tyt/static/css/2.f45d810d.chunk.css"
+  },
+  {
+    "revision": "18db24765f478187e426467e8d87d987",
+    "url": "/Table_tyt/index.html"
+  }
+];
